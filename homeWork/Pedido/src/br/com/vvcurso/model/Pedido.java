3d1ad8450e9a199957ef04442f49f24cc7e849d8
EMERSON RIOJI KOIKE <2102152932@viavarejo.br>
2019-03-16 13:10:17 -0300

    Meu segundo commit

---
+++ homeWork/Pedido/src/br/com/vvcurso/model/Pedido.java
@@ -0,0 +1,46 @@
+package br.com.vvcurso.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Pedido extends Base {
+
+	private String nomeCliente;
+	private String nomeFilial;
+	private List<ItemPedido> itens = new ArrayList<ItemPedido>();
+
+	public String getNomeCliente() {
+		return nomeCliente;
+	}
+
+	public void setNomeCliente(String nomeCliente) {
+		this.nomeCliente = nomeCliente;
+	}
+
+	public String getNomeFilial() {
+		return nomeFilial;
+	}
+
+	public void setNomeFilial(String nomeFilial) {
+		this.nomeFilial = nomeFilial;
+	}
+
+	public List<ItemPedido> getItens() {
+		return itens;
+	}
+
+	public void setItens(List<ItemPedido> listaItem) {
+		this.itens = listaItem;
+	}
+
+	public double getValor() {
+		// todo implementar busca para retorno
+		double valor = 0.0;
+		for (ItemPedido itemPedido : itens) {
+			valor = valor  + (itemPedido.getValor() * itemPedido.getQuantidade());
+		}
+		return valor;
+	}
+
+	
+}
